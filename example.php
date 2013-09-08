25aae0627d90d63bad2040b2c36da902c31a651f
Michael Kehoe <build@panda.micko.dyndns.org>
2013-09-08 21:45:01 +1000

    Initial commit

--- example.php
+++ example.php
@@ -1,4 +1,29 @@
 <?php
+/* This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Cisco Copy Config  Copyright (C) 2004  Max Schmind
+
+Original code by Max Schmind
+Original code can be found at: http://www.phpclasses.org/package/1575-PHP-Backup-Cisco-router-configuration-using-SNMP.html
+
+Modified code by Michael Kehoe
+Code can be found at: https://github.com/michael-kehoe/cisco-copy
+
+Changes to this code include: 
+ * Slight refactor of examples
+*/
+
 require_once('CiscoConfig.php');
 
 $CiscoDevice = '192.168.0.3';
